1cd96c7bfc65c8d034afbc09bbc7a58522ebcbde
Mini-Sylar <75648051+Mini-Sylar@users.noreply.github.com>
2022-09-06 12:06:43 +0000

    split into functions

--- Algorithms/Minutiae/Minutiae_OBJ.py
+++ Algorithms/Minutiae/Minutiae_OBJ.py
@@ -1,34 +1,41 @@
 from matplotlib import pyplot as plt
 
 from Algorithms.Minutiae.Libs.basics import load_image, display_image
-from Algorithms.Minutiae.Libs.edges import edge_processing
 from Algorithms.Minutiae.Libs.enhancing import enhance_image
-from Algorithms.Minutiae.Libs.minutiae import process_minutiae, generate_tuple_profile, plot_minutiae
+from Algorithms.Minutiae.Libs.minutiae import process_minutiae, generate_tuple_profile
 
 
 class Minutiae:
+    def __init__(self):
+        self.thinned_image = None
+        self.terminations_profile = None
+        self.minutiae_profile = None
+
     def detectAndComputeMinutiae(self, image_path):
-        #         Load images
+        # Load images
         image_raw = load_image(image_path, gray=True)
-        enhanced_image = enhance_image(image_raw, skeletonise=True)
-        # # display_image(enhanced_image,title="Enhanced Image")
-        minutiae = process_minutiae(enhanced_image)
+        self.thinned_image = enhance_image(image_raw, skeletonise=True)
+        minutiae = process_minutiae(self.thinned_image)
+        # Split Minutiae into terminations and bifurcations
         terminations, bifurcation = minutiae
         # Generate Tuple Profile Here
-        terminations_profile = generate_tuple_profile(terminations)
-        minutiae_profile = generate_tuple_profile(bifurcation)
-        # Generate Axis Here
+        self.terminations_profile = generate_tuple_profile(terminations)
+        self.minutiae_profile = generate_tuple_profile(bifurcation)
+        # Draw Graph here
+        self.plot_terminations_bifurcations()
+
+    def plot_terminations_bifurcations(self):
         fig, ax = plt.subplots(1, 1)
         # Show images on Axis
-        for y, x in terminations_profile.keys():
+        for y, x in self.terminations_profile.keys():
             termination = plt.Circle((x, y), radius=1, linewidth=2, color='red', fill=False)
             ax.add_artist(termination)
-            plt.imshow(enhanced_image)
-        for y, x in minutiae_profile.keys():
-            bifurcation = plt.Circle((x, y), radius=1, linewidth=2, color='green', fill=False)
+            plt.imshow(self.thinned_image)
+        for y, x in self.minutiae_profile.keys():
+            bifurcation = plt.Circle((x, y), radius=1, linewidth=2, color='blue', fill=False)
             ax.add_artist(bifurcation)
-            plt.imshow(enhanced_image)
-        display_image(enhanced_image,title="Terminations & Bifurcations")
+            plt.imshow(self.thinned_image)
+        display_image(self.thinned_image, title="Terminations & Bifurcations")
 
 
 fm = Minutiae()
